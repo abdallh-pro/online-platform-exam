fb79a9627338a376c7cfd07be118550c6bf53301
abdallh-pro <otakubenomar34@gmail.com>
2026-04-23 16:05:05 +0100

    .

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const connectDB = require('./config/database');
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+    })
+    .catch(err => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
+    });
